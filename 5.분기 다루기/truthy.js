b3a1292ec5b71faef55d052bccac39b0a9994a18
이상윤 <lee@isang-yuns-MacBook-Pro.local>
2023-06-02 10:51:39 +0900

    add: truthy & falsy

---
+++ 5.분기 다루기/truthy.js
@@ -0,0 +1,26 @@
+//https://developer.mozilla.org/ko/docs/Glossary/Truthy
+//https://developer.mozilla.org/ko/docs/Glossary/Falsy
+
+if('string'.length > 0){}
+
+if(!isNaN(10)){}
+
+// if(boolean === true){}
+
+//case_02
+function printName(name){
+    if(name === undefined || name === null){
+        return '사람이 없네요.';
+    }
+
+    return '안녕하세요' + name + '님';
+}
+
+//위 코드를 좀 더 간단하게 작성해 봅시다. null, undefined는 falsy 값 입니다. 
+
+function printName(name){
+    if(name){
+        return '사람이 없네요';
+    }
+    return '안녕하세요' + name + '님';
+}
