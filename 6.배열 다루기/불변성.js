d5b8dea15064e9c967ec2ef2a684d802000b4157
이상윤 <zaar625@gmail.com>
2023-06-04 22:49:23 +0900

    add: 불변성

---
+++ 6.배열 다루기/불변성.js
@@ -0,0 +1,21 @@
+/**
+ * 불변성
+ * 1. 배열을 복사합니다.
+ * 2. 새로운 배열을 반환하는 메서드들을 활용합니다.
+ * 3. 새로운 배열을 반환하는 메서드는 무엇들이 있을까요?
+ */
+// 
+
+const originArray = ['123', '456', '789'];
+
+const newArray = originArray;
+
+const newArray2 = [...originArray]
+
+originArray.push(10);
+originArray.push(11);
+originArray.push(12);
+originArray.unshift(0);
+
+console.log(newArray);
+
