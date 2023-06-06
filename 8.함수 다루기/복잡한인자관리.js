16f823def06c6152e2460e2c2215a7907a8ca6af
이상윤 <zaar625@gmail.com>
2023-06-06 20:53:34 +0900

    add: 복잡한 인자 관리하기

---
+++ 8.함수 다루기/복잡한인자관리.js
@@ -0,0 +1,64 @@
+// 복잡한 인자 관리하기
+// 많은 인자는 관리가 매우 어렵습니다. 하지만 무엇보다 중요한건 맥락을 유추할 수 있는 네이밍이 중요합니다.
+
+function toggleDisplay(isToggle){
+    //some code
+}
+
+function sum(sum1, sum2){
+    //...
+}
+
+
+function genRandomNumber(min, max) {
+    //...
+}
+
+function time(start, stop, end) {
+    //...
+}
+
+function genSquare(top, right, bottom, left) {
+    //...
+}
+
+//case_02 맥락을 알기 어려운 케이스입니다. 
+function createCar(name, brand, color, type) {
+    return {
+        name, 
+        brand,
+        color,
+        type
+    }
+}
+
+/**
+ * 위 코드를 개선해봅시다.
+ * 객체로 인자를 받아올 수 있습니다.  
+ * */ 
+
+function createCar(name, {brand, color, type}) {
+    return {
+        name, 
+        brand,
+        color,
+        type
+    }
+}
+createCar('차량 이름', {
+    type:''
+})
+
+// 복잡한 인자 관리하기
+
+function createCar({name, brand, color, type}) {
+    if(!name) {
+        throw new Error('name is a required');
+    }
+
+    if(!brand) { 
+        throw new Error('brand is a required')
+    }
+}
+
+createCar({name:'CAR', type:'SUV'}) // brand is a required
