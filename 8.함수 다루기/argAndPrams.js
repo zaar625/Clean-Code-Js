c3e28668f15b13ae5d97895f0c954e5005a4c88a
이상윤 <zaar625@gmail.com>
2023-06-06 20:34:39 +0900

    add: argument vs parameter

---
+++ 8.함수 다루기/argAndPrams.js
@@ -0,0 +1,26 @@
+// argument vs parameter
+
+function example(parameter) {
+    console.log(parameter);
+}
+
+const argument = 'foo';
+
+example(argument);
+
+/**
+ * Parameter (FormalParameter)
+ * 형식을 갖춘, 매개변수
+ */
+
+function axios(url){
+    //some code
+}
+
+/**
+ * Argument(Actual Parameter)
+ * 
+ * 실제로 사용되는 , 인자 or 인수
+ */
+
+axios('http://github.com')
