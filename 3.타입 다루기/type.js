7b2c9b58707f7cff28808c9022f88b76f696893c
이상윤 <zaar625@gmail.com>
2023-06-29 12:23:19 +0900

    fix:section3_타입검사

--- 3.타입 다루기/type.js
+++ 3.타입 다루기/type.js
@@ -6,8 +6,10 @@ console.log(typeof myFunction) // function
 const str = new String('문자열')
 console.log(typeof str) // object
 
-//위 에서 보는바와 같이 PRIMITIVE는 타입을 감별하기가 용이하지만, REFERENCE의 경우 object로 반환되기 때문에 감별하기 어렵습니다.
-//동적으로 변하는 언어이기 때문에 타입까지 동적입니다. 
+//위 에서 보는바와 같이 PRIMITIVE는 타입을 감별하기가 용이하지만, REFERENCE의 경우 object로 반환되기 때문에 감별하기 어렵습니다. -> typeof는 만능이 아니다.
+//자바스크립트는 동적으로 변하는 언어이기 때문에 타입까지 동적입니다. 
+
+typeof null //'object'
 
 //instance
 function Person(name, age) {
@@ -33,5 +35,7 @@ console.log(arr instanceof Object) //true
 console.log(func instanceof Object) //true
 console.log(date instanceof Object)// true
 
-//? 아래코드는 아직은 뭔말인지 잘 모르겠다. 
+//위 코드의 문제점을 아래의 형식으로 파악할 수 있습니다. 
 console.log(Object.prototype.toString.call(date)) // [object Date]
+console.log(Object.prototype.toString.call(func)) // [object Function]
+console.log(Object.prototype.toString.call(arr)) // [object Array]
